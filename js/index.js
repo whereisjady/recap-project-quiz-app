aad27bb4e0b12e190874cacc4cd5a0d6ca839574
Jady le Bars <js.lebars@gmail.com>
2024-11-15 12:08:49 +0100

    created different js files

--- js/index.js
+++ js/index.js
@@ -7,11 +7,3 @@ bookmarkButton.addEventListener("click", () => {
   const icon = bookmarkButton.querySelector(".bookmark-icon");
   icon.classList.toggle("bookmarked");
 });
-
-// Dark mode toggle
-const bodyElement = document.querySelector('[data-js="body"]');
-const darkModeButton = document.querySelector('[data-js="dark-mode"]');
-
-darkModeButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-});
